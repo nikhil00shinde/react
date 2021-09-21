3e3c738dded1f9d1de63d8fc622c153dbd1af46a
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-09-21 11:31:44 +0530

    libraries details

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+//Libraries
+
+import React from 'react'; // creation logic (logic of react that is components banana/dikhna)
+
+import ReactDOM from 'react-dom'; // render logic (shadow dom)
+
+//App is a component we have imported
 import App from './App';
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App />, //ek component
+  document.getElementById('root') // aur ek jagah jha us component ko dikhna hain
 );
