36e972eacca6f8da96c07d2bbe7ec31921280191
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-09-22 03:25:45 +0530

    todo app

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,9 +1,57 @@
-let App = ()=> {
-  return (
-    <div>
-      <h1>This is our first react app.</h1>
-    </div>
-  );
+import React from "react";
+
+
+
+class App extends React.Component{
+     state = {
+        tasks:["make coffee","make note"],
+        currInput:""
+     }
+
+     render = () =>{
+
+       return (
+
+         <div>
+
+           {/* jab bhi hum kuch bhi input tag mei type karte hain toh re-render hota hain */}
+
+           <input
+
+             //  on typing
+
+            onChange = {(e)=>{
+
+              // to show on screen of input tag
+              this.setState({currInput:e.currentTarget.value})
+
+            }}
+
+            onKeyDown = {(e)=>{
+              if(e.key == 'Enter')
+              {
+                this.setState({
+                  //hum directly push nhi kar sakte array mai toh humhe es prakar se karna padega
+                  tasks:[...this.state.tasks,this.state.currInput],
+                  currInput:""
+                })
+              }
+            }}
+            
+           value = {this.state.currInput}
+           />
+
+           {/* adding the notes */}
+           <ul>
+            {/* map function return array toh array apne app UI pe add ho jaega */}
+             {this.state.tasks.map((el)=>{
+                 return <li>{el}</li>
+             })}
+
+           </ul>
+         </div>
+       )
+     }
 }
 
 export default App;
--- demo/src/index.js
+++ demo/src/index.js
@@ -6,23 +6,12 @@ import React from 'react'; // creation logic (logic of react that is components 
 import ReactDOM from 'react-dom'; // render logic (shadow dom)
 
 //App is a component we have imported
-// import App from './App';
+import App from './App';
 
-import MyComp from './MyComp';
+
 
 
 ReactDOM.render(
-  <div>
-     <MyComp />
-     <hr/>
-     <MyComp />
-     <hr/>
-     <MyComp />
-     <hr/>
-     <MyComp />
-     <hr/>
-     <MyComp />
-  </div>
-   , //ek component (hota hain)
-  document.querySelector('#root') // aur ek jagah jha us component ko dikhna hain
+  <App/>, 
+  document.querySelector('#root') 
 );
